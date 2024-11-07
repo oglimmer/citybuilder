a44d564ce3f7ff2815bab44934cb21f0081471d3
Oli Zimpasser <oglimmer@gmail.com>
2024-11-07 22:07:52 +0100

    More translations

--- client/game/I18n.js
+++ client/game/I18n.js
@@ -80,157 +80,157 @@ var I18n = {
 		fieldType : ['U', 'H', 'R', 'G', 'C', 'J', 'E', 'P', 'X'],
 		fieldTypeLong : ['Occupied', 'House', 'Restaurant', 'Groceries', 'Clothing', 'Jewelery', 'Electronic', 'Public Services', 'Criminals'],
 
-		c_all_housetypes : 'alle Haustypen',
+		c_all_housetypes : 'all building types',
 
-		c0_1 : 'Italienisches Bistro',
-		c0_2 : 'Baue ein "Italienisches Bistro" anstelle eines beliebigen Wohnhauses.',
+		c0_1 : 'Italian Bistro',
+		c0_2 : 'Build an "Italian Bistro" instead of any residential building.',
 
-		c1_1 : 'Italienisches Restaurant',
-		c1_2 : 'Baue ein "Italienisches Restaurant" anstelle eines beliebigen Wohnhauses.',
+		c1_1 : 'Italian Restaurant',
+		c1_2 : 'Build an "Italian Restaurant" instead of any residential building.',
 
-		c2_1 : 'Nobel Italiener',
-		c2_2 : 'Baue ein "Italienisches Nobelrestaurant" anstelle eines beliebigen Wohnhauses.',
+		c2_1 : 'Fine Italian Dining',
+		c2_2 : 'Build a "Fine Italian Restaurant" instead of any residential building.',
 
-		c3_1 : 'Griechisches Restaurant',
-		c3_2 : 'Bau ein "Griechisches Restaurant" anstelle eines beliebigen Wohnhauses,',
+		c3_1 : 'Greek Restaurant',
+		c3_2 : 'Build a "Greek Restaurant" instead of any residential building.',
 
-		c4_1 : 'Chinesischer Schnellimbiss',
-		c4_2 : 'Baue einen "Chinesischen Schnellimbiss" anstelle eines beliebigen Wohnhauses.',
+		c4_1 : 'Chinese Fast Food',
+		c4_2 : 'Build a "Chinese Fast Food" instead of any residential building.',
 
-		c5_1 : 'Mongolischer Grill',
-		c5_2 : 'Baue einen "Mongolischen Grill" anstelle eines beliebigen Wohnhauses.',
+		c5_1 : 'Mongolian Grill',
+		c5_2 : 'Build a "Mongolian Grill" instead of any residential building.',
 
-		c6_1 : 'Steakhaus',
-		c6_2 : 'Baue ein "Steakhaus" anstelle eines beliebigen Wohnhauses.',
+		c6_1 : 'Steakhouse',
+		c6_2 : 'Build a "Steakhouse" instead of any residential building.',
 
-		c7_1 : 'Haute cuisine',
-		c7_2 : 'Baue ein "Haute cuisine Restaurant" anstelle eines beliebigen Wohnhauses.',
+		c7_1 : 'Haute Cuisine',
+		c7_2 : 'Build a "Haute Cuisine Restaurant" instead of any residential building.',
 
 		c8_1 : 'McDonalds',
-		c8_2 : 'Baue ein "McDonalds Restaurant" anstelle eines beliebigen Wohnhauses.',
+		c8_2 : 'Build a "McDonalds Restaurant" instead of any residential building.',
 
 		c9_1 : 'Burger King',
-		c9_2 : 'Baue ein "Burger King Restaurant" anstelle eines beliebigen Wohnhauses.',
+		c9_2 : 'Build a "Burger King Restaurant" instead of any residential building.',
 
 		c10_1 : 'KFC',
-		c10_2 : 'Baue ein "KFC Restaurant" anstelle eines beliebigen Wohnhauses.',
+		c10_2 : 'Build a "KFC Restaurant" instead of any residential building.',
 
-		c100_1 : 'Gemüseladen',
-		c100_2 : 'Baue einen "Gemüseladen" anstelle eines beliebigen Wohnhauses.',
+		c100_1 : 'Vegetable Store',
+		c100_2 : 'Build a "Vegetable Store" instead of any residential building.',
 
 		c101_1 : 'Aldi',
-		c101_2 : 'Baue einen "Aldi" anstelle eines beliebigen Wohnhauses.',
+		c101_2 : 'Build an "Aldi" instead of any residential building.',
 
-		c102_1 : 'Netto',
-		c102_2 : 'Baue einen "Netto Markt" anstelle eines beliebigen Wohnhauses.',
+		c102_1 : 'Dollar General',
+		c102_2 : 'Build a "Dollar General Market" instead of any residential building.',
 
-		c103_1 : 'Lidl',
-		c103_2 : 'Baue einen "Lidl" anstelle eines beliebigen Wohnhauses.',
+		c103_1 : 'Trader Joe\'s',
+		c103_2 : 'Build a "Trader Joe\'s" instead of any residential building.',
 
-		c104_1 : 'Real Super Store',
-		c104_2 : 'Baue einen "Real Super Store" anstelle eines beliebigen Wohnhauses.',
+		c104_1 : 'Walmart',
+		c104_2 : 'Build a "Walmart Store" instead of any residential building.',
 
-		c105_1 : 'Reformhaus',
-		c105_2 : 'Baue ein "Reformhaus" anstelle eines beliebigen Wohnhauses.',
+		c105_1 : 'Whole Foods Market',
+		c105_2 : 'Build a "Whole Foods Market" instead of any residential building.',
 
-		c106_1 : 'Tegut',
-		c106_2 : 'Baue einen "Tegut Markt" anstelle eines beliebigen Wohnhauses.',
-	
-		c200_1 : 'Kik',
-		c200_2 : 'Baue einen "Kik Kleidermarkt" anstelle eines beliebigen Wohnhauses.',
+		c106_1 : 'Sprouts Farmers Market',
+		c106_2 : 'Build a "Sprouts Farmers Market" instead of any residential building.',
 
-		c201_1 : 'Adler',
-		c201_2 : 'Baue einen "Adler Kleidermarkt" anstelle eines beliebigen Wohnhauses.',
+		c200_1 : 'Ross',
+		c200_2 : 'Build a "Ross Clothing Market" instead of any residential building.',
 
-		c202_1 : 'C&A',
-		c202_2 : 'Baue einen "C&A Kleidermarkt" anstelle eines beliebigen Wohnhauses.',
+		c201_1 : 'Target',
+		c201_2 : 'Build an "Target Clothing Market" instead of any residential building.',
+
+		c202_1 : 'Nordstrom',
+		c202_2 : 'Build a "Nordstrom Clothing Market" instead of any residential building.',
 
 		c203_1 : 'H&M',
-		c203_2 : 'Baue einen "H&M Kleidergeschäft" anstelle eines beliebigen Wohnhauses.',
+		c203_2 : 'Build an "H&M Clothing Store" instead of any residential building.',
 
-		c204_1 : 'D&G',
-		c204_2 : 'Baue einen "D&G Kleidergeschäft" anstelle eines beliebigen Wohnhauses.',
+		c204_1 : 'Dolce & Gabbana',
+		c204_2 : 'Build a "Dolce & Gabbana Clothing Store" instead of any residential building.',
 
 		c205_1 : 'Chanel',
-		c205_2 : 'Baue ein "Chanel Boutique" anstelle eines beliebigen Wohnhauses.',
+		c205_2 : 'Build a "Chanel Boutique" instead of any residential building.',
 
-		c206_1 : 'Ulla Popken',
-		c206_2 : 'Baue einen "Ulla Popken Kleidergeschäft" anstelle eines beliebigen Wohnhauses.',
+		c206_1 : 'Macy\s',
+		c206_2 : 'Build an "Macy\s Clothing Store" instead of any residential building.',
 
-		c207_1 : 'Breuninger',
-		c207_2 : 'Baue einen "Breuninger Kleidergeschäft" anstelle eines beliebigen Wohnhauses.',
+		c207_1 : 'Gap',
+		c207_2 : 'Build a "Gap Clothing Store" instead of any residential building.',
 
 		c208_1 : 'Abercrombie & Fitch',
-		c208_2 : 'Baue einen "Abercrombie & Fitch" anstelle eines beliebigen Wohnhauses.',
+		c208_2 : 'Build an "Abercrombie & Fitch" instead of any residential building.',
 
-		c300_1 : 'Bijou Brigitte',
-		c300_2 : 'Baue einen "Bijou Brigitte" anstelle eines beliebigen Wohnhauses.',
+		c300_1 : 'Claire\'s',
+		c300_2 : 'Build a "Claire\'s" instead of any residential building.',
 
-		c301_1 : 'Juwelier',
-		c301_2 : 'Baue einen "Juwelier" anstelle eines beliebigen Wohnhauses.',
+		c301_1 : 'Jeweler',
+		c301_2 : 'Build a "Jeweler" instead of any residential building.',
 
 		c302_1 : 'Tiffany & Co',
-		c302_2 : 'Baue einen "Tiffany & Co" anstelle eines beliebigen Wohnhauses.',
+		c302_2 : 'Build a "Tiffany & Co" instead of any residential building.',
 
-		c400_1 : 'TV & Hifi',
-		c400_2 : 'Baue einen "TV & Hifi - Laden" anstelle eines beliebigen Wohnhauses.',
+		c400_1 : 'TV & Hi-Fi',
+		c400_2 : 'Build a "TV & Hi-Fi Store" instead of any residential building.',
 
-		c401_1 : 'Elektronikmarkt',
-		c401_2 : 'Baue einen "Elektronikmarkt" anstelle eines beliebigen Wohnhauses.',
+		c401_1 : 'Electronics Market',
+		c401_2 : 'Build an "Electronics Market" instead of any residential building.',
 
-		c500_1 : 'Gentrifizierung',
-		c500_2 : 'Wähle ein Wohnhaus das gentrifiziert werden soll.',
+		c500_1 : 'Gentrification',
+		c500_2 : 'Select a residential building to be gentrified.',
 
-		c501_1 : 'Bevölkerungszuwachs',
-		c501_2 : 'Wähle ein Wohnhaus das mehr Bevökerung bekommen soll.',
+		c501_1 : 'Population Growth',
+		c501_2 : 'Select a residential building to receive additional population.',
 
-		c502_1 : 'Stadtwachsum',
-		c502_2 : 'Die Stadt wächst um 3x3 Blöcke.',
+		c502_1 : 'City Expansion',
+		c502_2 : 'The city expands by 3x3 blocks.',
 
-		c503_1 : 'Stadtwachsum',
-		c503_2 : 'Die Stadt wächst um 3x3 Blöcke.',
+		c503_1 : 'City Expansion',
+		c503_2 : 'The city expands by 3x3 blocks.',
 
-		c504_1 : 'Stadtwachsum',
-		c504_2 : 'Die Stadt wächst um 3x3 Blöcke.',
+		c504_1 : 'City Expansion',
+		c504_2 : 'The city expands by 3x3 blocks.',
 
-		c505_1 : 'Umzug',
-		c505_2 : 'Wähle ein Wohnhaus in dem Leute umziehen sollen. Manchmal bauen Sie auch einen anderen Haustypen.',
+		c505_1 : 'Relocation',
+		c505_2 : 'Select a residential building where people should move. They may build a different house type.',
 
-		c600_1 : 'Polizeistation',
-		c600_2 : 'Baue eine "Polizeistation" anstelle eines beliebigen Wohnhauses.',
+		c600_1 : 'Police Station',
+		c600_2 : 'Build a "Police Station" instead of any residential building.',
 
-		c601_1 : 'Krankenhaus',
-		c601_2 : 'Baue eine "Krankenhaus" anstelle eines beliebigen Wohnhauses.',
+		c601_1 : 'Hospital',
+		c601_2 : 'Build a "Hospital" instead of any residential building.',
 
-		c602_1 : 'Feuerwehrstation',
-		c602_2 : 'Baue eine "Feuerwehrstation" anstelle eines beliebigen Wohnhauses.',
+		c602_1 : 'Fire Station',
+		c602_2 : 'Build a "Fire Station" instead of any residential building.',
 
-		c700_1 : 'Spaß am Kochen',
-		c700_2 : 'Die Leute haben wieder Spaß am Kochen. Wähle ein Restaurant und entferne es.',
+		c700_1 : 'Cooking Fun',
+		c700_2 : 'People are excited about cooking again. Choose a restaurant and remove it.',
 
-		c701_1 : 'Kleiderpleite',
-		c701_2 : 'Die Leute haben kein Geld mehr für Kleider. Wähle ein Kleidergeschäft und entferne es.',
+		c701_1 : 'Clothing Bankruptcy',
+		c701_2 : 'People can\'t afford clothes. Choose a clothing store and remove it.',
 
-		c702_1 : 'Supermarktpleite',
-		c702_2 : 'Die Leute kaufen nichts mehr. Wähle einen Supermarkt und entferne ihn.',
+		c702_1 : 'Supermarket Bankruptcy',
+		c702_2 : 'People stop shopping. Choose a supermarket and remove it.',
 
-		c703_1 : 'No diamonds',
-		c703_2 : 'Die Leute kaufen keine Diamanten mehr. Wähle einen Juwelier und entferne ihn.',
+		c703_1 : 'No Diamonds',
+		c703_2 : 'People stop buying diamonds. Choose a jeweler and remove it.',
 
-		c704_1 : 'Elektronikmarktpleite',
-		c704_2 : 'Die Leute kaufen keine Elektronikprodukte mehr. Wähle einen Elektronikmarkt und entferne ihn.',
+		c704_1 : 'Electronics Market Bankruptcy',
+		c704_2 : 'People stop buying electronics. Choose an electronics market and remove it.',
 
-		c705_1 : 'Haushaltsdefizit',
-		c705_2 : 'Kein Geld im öffentlichen Haushalt. Wähle ein Polizeitrevier, Feuerwehrstation oder Krankenhaus und entferne es.',
+		c705_1 : 'Public Budget Deficit',
+		c705_2 : 'No money in the public budget. Choose a police station, fire station, or hospital and remove it.',
 
-		c800_1 : 'Dealer auf dem Spielplatz',
-		c800_2 : 'Einem Wohnfeld deiner Wahl wird zum Umschlagsplatz für Drogendealer. Kann nur auf ein {9,10} Wohnfeld gespielt werden.',
+		c800_1 : 'Dealer on Playground',
+		c800_2 : 'A residential lot of your choice becomes a drug dealer hotspot. Can only be played on a {9,10} residential lot.',
 
-		c801_1 : 'Bordell',
-		c801_2 : 'In einem Wohnfeld deiner Wahl wird ein Bordell eröffnet. Kann nur auf ein {7,10} Wohnfeld gespielt werden.',
+		c801_1 : 'Massage Parlor',
+		c801_2 : 'A Massage Parlor opens in a residential lot of your choice. Can only be played on a {7,10} residential lot.',
 
-		c802_1 : 'Edel Bordell',
-		c802_2 : 'In einem Wohnfeld deiner Wahl wird ein Edel Bordell eröffnet. Kann nur auf ein {1,4} Wohnfeld gespielt werden.',
+		c802_1 : 'High-Class Escort Place',
+		c802_2 : 'A high-class Escort Place opens in a residential lot of your choice. Can only be played on a {1,4} residential lot.',
 
 		help_0 : 'Here you can find information how the game works.<br/><br/>This is a multi-player computer-based card game.<br/><br/>You can either join a game somebody else created or create a game by yourself. Either way you have to put your name into the text field.',
 		help_1 : 'You created a new game.<br/><br/>Now wait for other players to join the game. If another player joined the game, his/her name will be added to the section `Players in the game`. When all players are listed there you have to press `Start Now`.<br/><br/>Even you can start a game with only you as a player, this doesn`t make much sense. So you should make sure that at least 2 players are in the game.<br/><br/>You can also defined how many rounds (also called months) this game should have. 12 months are a good starting point. A game with 12 months should take ~20-30 minutes.',
